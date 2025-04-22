2f0a9db294f4a31f91e9420e9dc9d00ab0245085
Dolamani Rohidas <dolamanirohidas696@gmail.com>
2025-04-23 00:37:13 +0530

    Error fix

--- src/app/admin/actions.ts
+++ src/app/admin/actions.ts
@@ -1,49 +1,46 @@
-"use server"
+"use server";
 
-import { auth, clerkClient } from "@clerk/nextjs/server"
-import { Roles } from "../../../types/globals"
-import { revalidatePath } from "next/cache"
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { Roles } from "../../../types/globals";
+import { revalidatePath } from "next/cache";
 
-export async function setRole(formData: FormData){
-    const { sessionClaims } = await auth()
+export async function setRole(formData: FormData) {
+  const { sessionClaims } = await auth();
 
-    if(sessionClaims?.metadata?.role !== "admin"){
-        throw new Error("You are not authorized to perform this action")
-    }
+  if (sessionClaims?.metadata?.role !== "admin") {
+    throw new Error("You are not authorized to perform this action");
+  }
 
-    const client = await clerkClient()
-    const id = formData.get("id") as string
-    const role = formData.get("role") as Roles
+  const client = await clerkClient();
+  const id = formData.get("id") as string;
+  const role = formData.get("role") as Roles;
 
-    try {
-        await client.users.updateUser(id, {
-            publicMetadata: { role }
-        })
-        revalidatePath("/admin")
-    } catch (error) {
-        throw new Error("Failed to set role")
-        
-    }
+  try {
+    await client.users.updateUser(id, {
+      publicMetadata: { role },
+    });
+    revalidatePath("/admin");
+  } catch {
+    throw new Error("Failed to set role");
+  }
 }
 
+export async function removeRole(formData: FormData) {
+  const { sessionClaims } = await auth();
 
-export async function removeRole(formData: FormData){
-    const { sessionClaims } = await auth()
+  if (sessionClaims?.metadata?.role !== "admin") {
+    throw new Error("You are not authorized to perform this action");
+  }
 
-    if(sessionClaims?.metadata?.role !== "admin"){
-        throw new Error("You are not authorized to perform this action")
-    }
+  const client = await clerkClient();
+  const id = formData.get("id") as string;
 
-    const client = await clerkClient()
-    const id = formData.get("id") as string
-
-    try {
-        await client.users.updateUser(id, {
-            publicMetadata: { role: null }
-        })
-        revalidatePath("/admin")
-    } catch (error) {
-        throw new Error("Failed to set role")
-        
-    }
+  try {
+    await client.users.updateUser(id, {
+      publicMetadata: { role: null },
+    });
+    revalidatePath("/admin");
+  } catch {
+    throw new Error("Failed to set role");
+  }
 }
